7925c1b762a7a016c7ab6380e85122f24062dcb7
2020wa86940 <167845555+2020wa86940@users.noreply.github.com>
2024-12-08 22:05:58 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "loadbucket123"
-    key = "first/terraform.tfstate"
+    key = "first/next/terraform.tfstate"
     region = "us-east-1"
   }
 }
